06ab8b1d01afc3c09e115ca6bb5e979955115255
zjgirl <35396196+zjgirl@users.noreply.github.com>
2021-01-24 21:32:51 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     // this is for prod config
+    
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
