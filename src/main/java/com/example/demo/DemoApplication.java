841af30703d8284effc17d5f1d033a89cee80345
czj <1425708375@qq.com>
2021-01-24 19:34:04 +0800

    remend demo

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+    // this is for prod
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
